a2adf45d11d7f5452a5bf9cce0d0a28624886c03
Gvova <gudyma.vova@gmail.com>
2021-12-12 05:58:17 +0200

    Update cartAjaxQtyUpdate.js

--- view/frontend/web/js/cartAjaxQtyUpdate.js
+++ view/frontend/web/js/cartAjaxQtyUpdate.js
@@ -1,32 +1,35 @@
 define([
     'jquery',
     'Magento_Checkout/js/action/get-totals',
-    'Magento_Customer/js/customer-data'
-], function ($, getTotalsAction, customerData) {
-    $('body').on('change', 'input[name$="[qty]"]', function(){
+    'Magento_Customer/js/customer-data',
+    'Magento_Ui/js/lib/view/utils/dom-observer'
+], function ($, getTotalsAction, customerData, domObserver) {
+    domObserver.get('input[name$="[qty]"]', function(){
+        $(this).on('change', function() {
         var form = $('form#form-validate');
-        $.ajax({
-            url: form.attr('action'),
-            data: form.serialize(),
-            showLoader: true,
-            success: function (res) {
-                var parsedResponse = $.parseHTML(res);
-                var result = $(parsedResponse).find("#form-validate");
-                var sections = ['cart'];
+            $.ajax({
+                url: form.attr('action'),
+                data: form.serialize(),
+                showLoader: true,
+                success: function (res) {
+                    var parsedResponse = $.parseHTML(res);
+                    var result = $(parsedResponse).find("#form-validate");
+                    var sections = ['cart'];
 
-                $("#form-validate").replaceWith(result);
+                    $("#form-validate").replaceWith(result);
 
-                // The mini cart reloading
-                customerData.reload(sections, true);
+                    // The mini cart reloading
+                    customerData.reload(sections, true);
 
-                // The totals summary block reloading
-                var deferred = $.Deferred();
-                getTotalsAction([], deferred);
-            },
-            error: function (xhr, status, error) {
-                var err = eval("(" + xhr.responseText + ")");
-                console.log(err.Message);
-            }
+                    // The totals summary block reloading
+                    var deferred = $.Deferred();
+                    getTotalsAction([], deferred);
+                },
+                error: function (xhr, status, error) {
+                    var err = eval("(" + xhr.responseText + ")");
+                    console.log(err.Message);
+                }
+            });
         });
     });
 });
